6b718bddf7547a9514ed5c4d4e18239a9119a21a
justoucha1 <justoucha46@gmail.com>
2018-04-27 13:59:00 -0300

    some commit

---
+++ fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main {
+
+
+ 
+
+
+}
+
+int fibonacci (int n) {
+	int i;
+
+	for(i=0; i<=n; i=i+1) {
+
+}
+
+int aux;
+	for(nosequeponer){
+	aux=x+y ;
+	x=y;
+	y=aux ;
+}
+
+
+
